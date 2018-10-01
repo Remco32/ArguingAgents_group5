6d79f198e23ef8e3279834ae75163e25c1a64e2e
jwdw <jwdwit@gmail.com>
2018-10-01 16:01:25 +0200

    finding most similar works, added converting script for testing

---
+++ Code/convert_json.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+
+input_file = "../Crawler/Crawled/ProconOrg/shortArguments/animalTesting.json"
+output_file = "../Crawler/Crawled_csv/ProconOrg/shortArguments/animaltesting.csv"
+
+with open(input_file) as f:
+    data = json.load(f)
+
+output = open(output_file, 'w')
+
+for x in data[1:]:
+    if 'Pro argument' in x:
+        output.write("pro,")
+        output.write(x['Pro argument'][0])
+        output.write('\n')
+    elif 'Con argument' in x:
+        output.write("con,")
+        output.write(x['Con argument'][0])
+        output.write('\n')
--- Code/doc2vec.py
+++ Code/doc2vec.py
@@ -2,24 +2,31 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from nltk.tokenize import word_tokenize
 
 
-data = ["Medical marijuana shows considerable promise in reducing chronic pain",
-        "As a physician, I have constantly searched for treatment options for my patients' chronic pain.",
-        "I had steadily reviewed the scientific literature on medical marijuana from the United States",
-        "The evidence is overwhelming that marijuana can relieve certain types of pain",
-        "Marijuana, or cannabis, as it is more appropriately called,\nhas been part of humanity's medicine chest for almost as long as history has\nbeen recorded"
-        ]
+# data = ["Medical marijuana shows considerable promise in reducing chronic pain",
+#         "As a physician, I have constantly searched for treatment options for my patients' chronic pain.",
+#         "I had steadily reviewed the scientific literature on medical marijuana from the United States",
+#         "The evidence is overwhelming that marijuana can relieve certain types of pain",
+#         "Marijuana, or cannabis, as it is more appropriately called,\nhas been part of humanity's medicine chest for almost as long as history has\nbeen recorded"
+#         ]
+
+data = []
+input_file = "./Crawler/Crawled_csv/ProconOrg/shortArguments/animaltesting.csv"
+for line in open(input_file, 'r'):
+    data.append(line.split(",")[1].rstrip())
+
+print(data)
 
 # TODO: filter out stop words using nltk
 tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(data)]
 
-epochs = 50
-vec_size = 10
-alpha = 0.025
+epochs = 500
+vec_size = 100
+alpha = 0.25
 
 def train(vec_size, alpha, epochs, tagged_data):
     model = Doc2Vec(vector_size=vec_size,  # size of the feature vector
                     alpha=alpha,  # initial learning rate
-                    min_alpha=0.00025,  # learning rate will linearly decrease to this during training
+                    min_alpha=0.00000025,  # learning rate will linearly decrease to this during training
                     min_count=1  # ignores words with frequency lower than min_count
                     )
 
@@ -36,8 +43,9 @@ def train(vec_size, alpha, epochs, tagged_data):
 train(vec_size, alpha, epochs, tagged_data)
 
 model= Doc2Vec.load("d2v.model")
-test_data = word_tokenize("there is evidence that marijuana is helpful in relieving certain types of pain".lower())
+test_data = word_tokenize("Animal testing has contributed to many life-saving cures and treatments".lower())
+test_vector = model.infer_vector(test_data)
 
-similar = model.docvecs.most_similar('1')
+similar = model.docvecs.most_similar([test_vector])
 print(data[int(similar[0][0])])
 
