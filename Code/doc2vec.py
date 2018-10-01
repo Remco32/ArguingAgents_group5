5a808817a5f861e26f66470465b035b03d11b3c9
jwdw <jwdwit@gmail.com>
2018-10-01 15:14:33 +0200

    update doc2vec

--- Code/doc2vec.py
+++ Code/doc2vec.py
@@ -9,34 +9,31 @@ data = ["Medical marijuana shows considerable promise in reducing chronic pain",
         "Marijuana, or cannabis, as it is more appropriately called,\nhas been part of humanity's medicine chest for almost as long as history has\nbeen recorded"
         ]
 
+# TODO: filter out stop words using nltk
 tagged_data = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[str(i)]) for i, _d in enumerate(data)]
 
-max_epochs = 100
-vec_size = 20
+epochs = 50
+vec_size = 10
 alpha = 0.025
 
-def train(vec_size, alpha, max_epochs, tagged_data):
-    model = Doc2Vec(vector_size=vec_size,
-                    alpha=alpha,
-                    min_alpha=0.00025,
-                    min_count=1,
-                    dm=1)
+def train(vec_size, alpha, epochs, tagged_data):
+    model = Doc2Vec(vector_size=vec_size,  # size of the feature vector
+                    alpha=alpha,  # initial learning rate
+                    min_alpha=0.00025,  # learning rate will linearly decrease to this during training
+                    min_count=1  # ignores words with frequency lower than min_count
+                    )
 
     model.build_vocab(tagged_data)
 
-    for epoch in range(max_epochs):
-            model.train(tagged_data,
-                        total_examples=model.corpus_count,
-                        epochs=model.iter)
-
-            model.alpha -= 0.0002
-            model.min_alpha = model.alpha
+    model.train(tagged_data,
+                total_examples=model.corpus_count,
+                epochs=epochs
+                )
 
     model.save("d2v.model")
-    print("Model Saved")
 
 
-train(vec_size, alpha, max_epochs, tagged_data)
+train(vec_size, alpha, epochs, tagged_data)
 
 model= Doc2Vec.load("d2v.model")
 test_data = word_tokenize("there is evidence that marijuana is helpful in relieving certain types of pain".lower())
