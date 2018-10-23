d26a29efcb4c15dc908075e9fe3cd443e4a6e9e9
Remco32 <remco32@gmail.com>
2018-10-23 17:54:52 +0200

    One more bug to fix, too tired now.

--- Code/combine_corpus.py
+++ Code/combine_corpus.py
@@ -1,12 +1,16 @@
 import os
+from os import listdir
 import shutil
+import re
 
 output_path = "../Crawler/Corpus/"
 corpusDirOne = "../Crawler/Corpus/Debatabase/"
 corpusDirTwo = "../Crawler/Corpus/ProconOrg"
 
-def combineCorpera():
-    intersection = findIntersectionTopics()
+def combineCorpera(intersectionBool):
+    intersection = []
+    if intersectionBool is True:
+        intersection = findIntersectionTopics()
 
     #Loop all directories
     for subdir, topicsCorpus, files in os.walk(corpusDirOne):
@@ -21,21 +25,47 @@ def combineCorpera():
             for topic in topicsCorpus:  # directory is not empty
                 if topic not in intersection:
                     shutil.move(subdir + '\\' + topic, output_path)
-        #else:
-            #combineDirectories()
 
-    return
+
+#For the corpera with intersection between them
+def combineRemainingCorpera():
+    #Move remaining corpera, which have intersection
+    for subdir1, topicsCorpus1, files1 in os.walk(corpusDirOne):
+        for subdir2, topicsCorpus2, files2 in os.walk(corpusDirTwo):
+            if topicsCorpus2 and 'longArguments' not in topicsCorpus2:
+                for topic1 in topicsCorpus1:
+                    for topic2 in topicsCorpus2:
+                        if topic1 == topic2:
+                            combineDirectories(subdir1, subdir2, topic1)
+    combineCorpera(False) #once more for the intersection
 
 #Combine directories into one single one per topic.
-def combineDirectories(dir1, dir2):
+def combineDirectories(dir1, dir2, topic):
+        #Count the amount of pros and cons in a corpus
+        amountOfProArguments = len(listdir(dir1+topic))/2 #Dir1 is a directory of the debatabase. The debatabase has the same number of pro as con arguments
+
+        #Loop through files in corpus 2 and
+        for subdir, topicsCorpus, files in os.walk(dir2 + '\\' + topic):
+            #Get number of the argument
+            for file in files:
+
+                firstChar = file[0]
+                number = (file[3:])
+                number = int(re.sub('\.txt$', '', number))
+                #increment number by amountOfProArguments
+                number += int(amountOfProArguments)
+                number = str(number)
+
+                #change the filename
+                if firstChar == 'c':
+                    os.rename(dir2 + '\\' + topic + '\\' + file, dir2 + '\\' + topic + '\\' + 'con' + number + '.txt')
+                if firstChar == 'p':
+                    os.rename(dir2 + '\\' + topic + '\\' + file, dir2 + '\\' + topic + '\\' + 'pro' + number + '.txt')
 
 
 
-            #Find both directories
-            #Count the highest value in corpus 1 for both pros and cons (i.e. con9.txt -> 9)
-            #Loop through files in corpus 2 and increment the filename by highest value previous corpus
+        #Move files from corpus one
 
-    return
 
 
 # Check if there are two corpera for one topic (same folder name)
@@ -57,4 +87,9 @@ def findIntersectionTopics():
     return list(set(topicsCorpOne) & set(topicsCorpTwo))
 
 
-combineCorpera()
+def main():
+    combineCorpera(True)
+    combineRemainingCorpera()
+
+#Run
+main()
