dd201efdcd279e3bd79d282afe355322cb41f948
Remco32 <remco32@gmail.com>
2018-10-23 15:03:37 +0200

    Function on finding intersecting topics

--- Code/combine_corpus.py
+++ Code/combine_corpus.py
@@ -1,20 +1,22 @@
 import os
 
 input_path = "../Crawler/Corpus/"
+corpusDirOne = "../Crawler/Corpus/Debatabase/"
+corpusDirTwo = "../Crawler/Corpus/ProconOrg"
 
 def combineCorpera():
-    #Check if there are two corpera for one topic (same folder name)
-    for subdir, dirs, files in os.walk(input_path):
-        if 'Debatabase' in dirs:
+    intersection = findIntersectionTopics()
 
-            for file in files:
-                print(file)
-                input_file = os.path.join(subdir, file)
-            new_output_path = ""  # clear the string since we loop
-            #new_output_path = output_path +
-#Count the highest value in corpus 1 for both pros and cons (i.e. con9.txt -> 9)
+    #Loop all directories
 
-#Loop through files in corpus 2 and increment the filename by highest value previous corpus
+    #Check if topic has intersection
+        #Find both directories
+
+    #Count the highest value in corpus 1 for both pros and cons (i.e. con9.txt -> 9)
+
+    #Loop through files in corpus 2 and increment the filename by highest value previous corpus
+
+    return
 
 
 
@@ -23,6 +25,24 @@ def combineDirectories():
 #Combine directories into one single one per topic.
 
     return
+
+#Check if there are two corpera for one topic (same folder name)
+#Return intersection
+def findIntersectionTopics():
+    topicsCorpTwo = [] #for if-statement
+    for subdir, dirs, files in os.walk(corpusDirOne):
+        if dirs: #directory is not empty
+            topicsCorpOne = dirs
+
+    for subdir, dirs, files in os.walk(corpusDirTwo):
+        if dirs and 'longArguments' not in dirs : #directory is not empty, and isn't root folder named longArguments (and not shortArguments)
+            if not topicsCorpTwo:
+                topicsCorpTwo = dirs
+            else:
+                topicsCorpTwo += dirs #Add two procon corpera together
+
+    #Find intersection of the sets, return this intersection
+    return list(set(topicsCorpOne) & set(topicsCorpTwo))
 
 
 combineCorpera()
